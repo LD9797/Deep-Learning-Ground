30b3e922f79ebb7dbec7afae867af747db41b05d
Luis Diego <ldiegohcr@gmail.com>
2022-08-16 17:41:22 -0600

    Code refactoring para reusabilidad

--- playground.py
+++ playground.py
@@ -10,16 +10,17 @@ def derivada_funcion_a(x, y):
     return x / math.sqrt(x**2 + y**2)
 
 
-def evaluar_grandiente_a(punto_0, punto_1):
-    xs = torch.linspace(-1, 1, steps=256)  # x1
-    ys = torch.linspace(-1, 1, steps=256)  # x2
+def evaluar_grandiente(punto_0, punto_1, funcion_derivada, arrange):
+    #  Arrange es de que valor a que valor se va a evaluar la funcion para graficarla
+    xs = torch.linspace(arrange[0], arrange[1], steps=256)  # x1
+    ys = torch.linspace(arrange[0], arrange[1], steps=256)  # x2
     x, y = torch.meshgrid(xs, ys, indexing='xy')
     z = torch.sqrt(x**2 + y**2)
     punto_x, punto_y = punto_0[0], punto_0[1]
     # X, Y, Z
-    vector_grandiente_punto_0 = [derivada_funcion_a(punto_x, punto_y), derivada_funcion_a(punto_y, punto_x), 0]
+    vector_grandiente_punto_0 = [funcion_derivada(punto_x, punto_y), funcion_derivada(punto_y, punto_x), 0]
     punto_x, punto_y = punto_1[0], punto_1[1]
-    vector_grandiente_punto_1 = [derivada_funcion_a(punto_x, punto_y), derivada_funcion_a(punto_y, punto_x), 0]
+    vector_grandiente_punto_1 = [funcion_derivada(punto_x, punto_y), funcion_derivada(punto_y, punto_x), 0]
     inicio = [0, 0, 0]
     ax = plt.axes(projection='3d')
     #  Se dibujan los vectores
@@ -34,4 +35,4 @@ def evaluar_grandiente_a(punto_0, punto_1):
 
 
 if __name__ == "__main__":
-    evaluar_grandiente_a([5.2, 6.4], [5.2, 2.3])
+    evaluar_grandiente([5.2, 6.4], [5.2, 2.3], derivada_funcion_a, [-1, 1])
