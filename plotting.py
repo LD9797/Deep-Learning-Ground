f8d2f7d2b7df0e32dc38a8effee3b1efdca02604
Luis Diego <ldiegohcr@gmail.com>
2022-09-29 22:39:27 -0600

    Ultimos commits para avanzar a TP1

--- plotting.py
+++ plotting.py
@@ -3,20 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-x = torch.linspace(-20, 20, steps=256)  # x1
-y = x ** 2
-z = 2 ** x
 
-fig, ax = plt.subplots()
+def t(n):
+    if n == 2:
+        return 2
+    if n == 3:
+        return 1
+    if n > 3:
+        return 2 * t(math.ceil((n+1)/2)) + t(math.ceil(n/2))
 
-ax.plot(x.numpy(), y.numpy(), linewidth=2.0)
-ax.plot(x.numpy(), z.numpy(), linewidth=2.0)
 
-ax.set(xlim=(-10, 10), xticks=np.arange(-10, 10),
-       ylim=(-1, 8), yticks=np.arange(0, 20))
+pairs = []
+pairs_plus_one = []
 
-ax.spines['left'].set_position(('data', 0))
-ax.spines['bottom'].set_position(('data', 0))
+for x in range(2, 100):
+    pairs.append([x, t(x)])
+    pairs_plus_one.append([x, t(x+1)])
 
+
+pairs = np.array(pairs)
+pairs_plus_one = np.array(pairs_plus_one)
+
+plt.scatter(pairs[:, 0], pairs[:, 1], s=5, alpha=0.5)
+plt.scatter(pairs_plus_one[:, 0], pairs_plus_one[:, 1], s=5, alpha=0.5)
 plt.show()
+
+
 
